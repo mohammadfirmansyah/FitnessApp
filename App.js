7d2bf15572e2ea31cd9a39deb9f3b1635c2a48ea
Firman <mohammadfirmansyah.dev@gmail.com>
2025-10-20 12:49:50 +0700

    added styling

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import FitnessWorld from './FitnessWorld'
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-     <FitnessWorld/> 
+ <FitnessWorld/>
     </SafeAreaView>
   );
 }
